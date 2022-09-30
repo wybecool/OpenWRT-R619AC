fe7188f14ba01ee5526b7c911a45cb7af35b1433
Mike-qian <mike1994976451@outlook.com>
2022-09-30 10:49:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,2 @@
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git libs https://github.com/openwrt/luci/tree/master/libs' feeds.conf.default
