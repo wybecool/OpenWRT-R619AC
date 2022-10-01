57e9c85fa7b966d708d9a18f1cf91408dd8041f6
Mike-qian <mike1994976451@outlook.com>
2022-10-01 17:31:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,4 @@
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
+#sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
+rm -rf ./smpackageluci-app-bypass/  
+git clone https://github.com/tianiue/luci-app-bypass ./smpackage/
