dc083c74d14e792db5e8d936e8bf48aa1eaf7676
Mike-qian <mike1994976451@outlook.com>
2022-10-02 07:57:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,5 @@
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
 
 
