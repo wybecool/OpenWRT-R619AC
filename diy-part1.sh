a463818e25ffe2f7129a9fc68866a506079be0ab
Mike-qian <mike1994976451@outlook.com>
2022-10-02 07:46:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,4 @@
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
-#rm -rf ./smpackage/luci-app-bypass/
-#git clone https://github.com/tianiue/luci-app-bypass ./smpackage/
+
 
