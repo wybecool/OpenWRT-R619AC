804ccf09854981660db70f8e9670805919dcb72b
Mike-qian <mike1994976451@outlook.com>
2022-10-05 20:38:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,3 @@
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
-
-
-sed -i '$a src-git coolsnowwolf https://github.com/coolsnowwolf/packages/tree/master/net' feeds.conf.default
