5877462a4bfab78f569632f3afb2bce0b3246d54
Mike-qian <mike1994976451@outlook.com>
2022-10-09 16:22:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,5 @@
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 sed -i '$a src-git lede https://github.com/coolsnowwolf/packages' feeds.conf.default
-sed -i '$a src-git redsocks2 https://github.com/sdhz153/redsocks2' feeds.conf.default
+#sed -i '$a src-git redsocks2 https://github.com/sdhz153/redsocks2' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
