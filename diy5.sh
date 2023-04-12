8df346c1466addd1122478b7df8f44171fdb3ea0
Mike-qian <mike1994976451@outlook.com>
2023-04-12 14:08:39 +0800

    Update diy5.sh

--- diy5.sh
+++ diy5.sh
@@ -1,2 +1,2 @@
-#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-src/gz openwrt_kiddin9 https://op.supes.top/packages/arm_cortex-a7_neon-vfpv4
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#src/gz openwrt_kiddin9 https://op.supes.top/packages/arm_cortex-a7_neon-vfpv4
