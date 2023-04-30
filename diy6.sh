88064ec352809d775d60f3e450fd7368e0f02451
Mike-qian <mike1994976451@outlook.com>
2023-05-01 04:26:51 +0800

    Update diy6.sh

--- diy6.sh
+++ diy6.sh
@@ -1,1 +1,2 @@
 #sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
