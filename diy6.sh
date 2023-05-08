d0085eb72864e25c0af749c262c86410ceb3bfd9
Mike-qian <mike1994976451@outlook.com>
2023-05-08 13:18:11 +0800

    Update diy6.sh

--- diy6.sh
+++ diy6.sh
@@ -4,10 +4,10 @@ sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' ./package/k
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' ./package/base-files/files/etc/shadow
 #添加第三方软件源
 sed -i 's/option check_signature 1/option check_signature 0/g' ./package/system/opkg/files/opkg-smime.conf
-sed -i 's/timezone:string:UTC'/timezone:string:CST-8'/g' ./package/base-files/files/etc/init.d/system
-#修改时区
-sed -i 's/zonename:string'/zonename:string:Asia'#/'Shanghai'/g' ./package/base-files/files/etc/init.d/system
 echo src/gz openwrt_kiddin9 https://op.supes.top/packages/arm_cortex-a7_neon-vfpv4 >> ./package/system/opkg/files/customfeeds.conf
+#修改时区
+sed -i 's/timezone:string:UTC'/timezone:string:CST-8'/g' ./package/base-files/files/etc/init.d/system
+sed -i 's/zonename:string'/zonename:string:Asia'#/'Shanghai'/g' ./package/base-files/files/etc/init.d/system
 #添加超频补丁,内核调教补丁#自动挂载硬盘补丁
 wget -P ./target/linux/ipq40xx/base-files/etc/init.d/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/set-irq-affinity
 wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
