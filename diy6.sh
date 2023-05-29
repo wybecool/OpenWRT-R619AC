a08d08dfd3be11fa3ac1b7e7f6aee564af7c07ff
Mike-qian <mike1994976451@outlook.com>
2023-05-30 01:59:19 +0800

    Update diy6.sh

--- diy6.sh
+++ diy6.sh
@@ -9,7 +9,7 @@ echo src/gz openwrt_kiddin9 https://op.supes.top/packages/arm_cortex-a7_neon-vfp
 sed -i 's/timezone:string:UTC'/timezone:string:CST-8'/g' ./package/base-files/files/etc/init.d/system
 sed -i 's/zonename:string'/zonename:string:Asia'#/'Shanghai'/g' ./package/base-files/files/etc/init.d/system
 #添加超频补丁,内核调教补丁#自动挂载硬盘补丁
-wget -P ./target/linux/ipq40xx/base-files/etc/init.d/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/set-irq-affinity
+#wget -P ./target/linux/ipq40xx/base-files/etc/init.d/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/set-irq-affinity
 wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
 wget -P ./target/linux/ipq40xx/patches-5.15/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/999-ipq40xx-unlock-cpu-frequency.patch
 chmod 0755 target/linux/ipq40xx/base-files/etc/init.d/set-irq-affinity
