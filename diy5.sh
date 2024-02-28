8e114642ad7d21d60a51ae8d60c0f2a8e44cb968
Mike-qian <mike1994976451@outlook.com>
2024-02-28 21:35:59 +0800

    Update diy5.sh

--- diy5.sh
+++ diy5.sh
@@ -1,1 +1,2 @@
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
