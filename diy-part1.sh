022aacddb526a336adf5a8d11b361d797f21a7d9
Mike-qian <mike1994976451@outlook.com>
2024-03-14 01:43:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,4 @@
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 sed -i '1i src-git kenzok81 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 sed -i '1i src-git kenzok82 https://github.com/kenzok8/small-package.git' feeds.conf.default
-sed -i '1i src-git kenzok83 https://github.com/kenzok8/small.git' feeds.conf.default
+#sed -i '1i src-git kenzok83 https://github.com/kenzok8/small.git' feeds.conf.default
