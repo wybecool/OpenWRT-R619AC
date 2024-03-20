ea643e279e30afee1f1065c6dcb80796dfde51d1
Mike-qian <mike1994976451@outlook.com>
2024-03-21 05:39:21 +0800

    Update diy5.sh

--- diy5.sh
+++ diy5.sh
@@ -1,5 +1,4 @@
 #sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
-sed -i '1i src-git kenzok81 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-sed -i '1i src-git kenzok82 https://github.com/kenzok8/small-package.git' feeds.conf.default
-#sed -i '1i src-git kenzok83 https://github.com/kenzok8/small.git' feeds.conf.default
+#sed -i '1i src-git kenzok81 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '1i src-git kenzok8 https://github.com/kenzok8/small-package.git' feeds.conf.default
