616aad39649949df23ccae5a15af7dd2c2d08fe2
Mike-qian <mike1994976451@outlook.com>
2024-11-21 20:35:22 +0800

    Update diy5.sh

--- diy5.sh
+++ diy5.sh
@@ -1,1 +1,1 @@
-sed -i '1i src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
